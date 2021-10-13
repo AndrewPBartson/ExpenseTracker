e8160718f888bac0c223571b4bf950802c713911
swatitayal <31080980+swatitayal@users.noreply.github.com>
2021-10-13 09:42:10 -0700

    Implemented clickable feature for Category Icons
    Implemented clickable feature for Category Icons so that they will read the value for category user has selected.

--- ExpenseTracker/ExpenseTracker/AddExpensePage.xaml.cs
+++ ExpenseTracker/ExpenseTracker/AddExpensePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExpenseTracker.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,8 +16,10 @@ namespace ExpenseTracker
         public AddExpensePage()
         {
             InitializeComponent();
+            CategoryIcon.getAllIcons();
         }
 
+        public string category;
         //protected override void OnAppearing()
         //{
         //    //Code to check if the values need to be pre-populated by reading from the file.
@@ -38,6 +41,81 @@ namespace ExpenseTracker
             var date = (DateTime)e.NewDate;
             Console.WriteLine("The date selected is: " + date);
         }
-       }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HomeIcon_Clicked(object sender, EventArgs e)
+        {
+            category = "Home";
+            IconsEnableDisable();
+            HomeIcon.BackgroundColor = Color.Aqua;
+        }
+
+        private void ShoppingIcon_Clicked(object sender, EventArgs e)
+        {
+            category = "Shopping";
+            IconsEnableDisable();
+            ShoppingIcon.BackgroundColor = Color.Aqua;
+        }
+
+        private void TravelIcon_Clicked(object sender, EventArgs e)
+        {
+            category = "Travel";
+            IconsEnableDisable();
+            TravelIcon.BackgroundColor = Color.Aqua;
+        }
+
+        private void FoodIcon_Clicked(object sender, EventArgs e)
+        {
+            category = "Food";
+            IconsEnableDisable();
+            FoodIcon.BackgroundColor = Color.Aqua;
+        }
+
+        private void EntertainmentIcon_Clicked(object sender, EventArgs e)
+        {
+            category = "Entertainment";
+            IconsEnableDisable();
+            EntertainmentIcon.BackgroundColor = Color.Aqua;
+        }
+
+        private void EducationIcon_Clicked(object sender, EventArgs e)
+        {
+            category = "Education";
+            IconsEnableDisable();
+            EducationIcon.BackgroundColor = Color.Aqua;
+        }
+
+        private void BillsIcon_Clicked(object sender, EventArgs e)
+        {
+            category = "Bills";
+            IconsEnableDisable();
+            BillsIcon.BackgroundColor = Color.Aqua;
+        }
+
+        private void GiftIcon_Clicked(object sender, EventArgs e)
+        {
+            category = "Gift";
+            IconsEnableDisable();
+            GiftIcon.BackgroundColor = Color.Aqua;
+
+        }
+
+        private void IconsEnableDisable()
+        {
+            HomeIcon.BackgroundColor = Color.LightGray;
+            ShoppingIcon.BackgroundColor = Color.LightGray;
+            TravelIcon.BackgroundColor = Color.LightGray;
+            FoodIcon.BackgroundColor = Color.LightGray;
+            EntertainmentIcon.BackgroundColor = Color.LightGray;
+            EducationIcon.BackgroundColor = Color.LightGray;
+            BillsIcon.BackgroundColor = Color.LightGray;
+            GiftIcon.BackgroundColor = Color.LightGray;
+
+        }
+    }
     
 }
