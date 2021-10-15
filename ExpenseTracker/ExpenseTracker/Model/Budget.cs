ece715ac1ccf3f6c0b43fd133ac87dfc29d6f49d
priya-kamble <priya90kamble@gmail.com>
2021-10-15 12:37:23 -0700

    1. Added LoginPage with buttons "Sign In" for valid user and "Sign Up" for new user.  2. Changed UserManager and FileManager to static class.  3. Created a static variable "LoggedInUser"  to hold current user data.

--- ExpenseTracker/ExpenseTracker/Model/Budget.cs
+++ ExpenseTracker/ExpenseTracker/Model/Budget.cs
@@ -6,31 +6,8 @@ namespace ExpenseTracker.Model
 {
     public class Budget
     {
-        public float BudgetGoalAmount { get; set; }
-        public List<Expenses> Expenses { get; private set; }
+        public decimal BudgetGoalAmount { get; set; }
+        public List<Expenses> ExpenseList { get; set; }
         public DateTime BudgetDate { get; set; }
-
-        // Buget should allow you to Add expense, Edit Expense and Delete Expense
-
-
-        public List<Expenses> AddExpense(Expenses expobj)
-        {
-           
-
-            return null;
-        }
-
-        public List<Expenses> EditExpense(Expenses expobj)
-        {
-            return null; 
-        }
-
-        public List<Expenses> DeleteExpense(Expenses expobj)
-        {
-           
-            return null;
-        }
-
-
     }
 }
